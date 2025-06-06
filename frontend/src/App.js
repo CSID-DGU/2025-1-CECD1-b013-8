a5568b731469eedb5cde89f923721f888a731315
Jinsoo Yoon <floreo1242@gmail.com>
2025-06-06 21:20:48 +0900

    자서전 PDF 뷰어 컴포넌트 제작
    - Button, Input 컴포넌트
    - react-pdf 라이브러리 설정

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,14 +2,16 @@ import React from "react";
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 
 import ApiTest from "./pages/ApiTest/ApiTest";
+import AutobioViewer from "./pages/AutobioViewer/AutobioViewer";
 
 function App() {
   return (
     <Router>
-        <Routes>
-          <Route path="/" element={<ApiTest />} />
-        </Routes>
-      </Router>
+      <Routes>
+        <Route path="/" element={<ApiTest />} />
+        <Route path="/view" element={<AutobioViewer />} />
+      </Routes>
+    </Router>
   );
 }
 
